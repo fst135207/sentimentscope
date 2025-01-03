435018743392a3f5f13ba8da0284f4deaf9a92ea
fst135207 <felix.stalder.bsu@gmail.com>
2025-01-03 15:55:14 +0100

    added firestore utility

---
+++ pages/api/sublabels.ts
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { db } from "@/utils/firebaseConfig";
+import { collection, getDocs } from "firebase/firestore";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "GET") {
+    try {
+      const labelsSnapshot = await getDocs(collection(db, "mainLabels"));
+      const labels = labelsSnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+
+      res.status(200).json(labels);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Failed to fetch labels" });
+    }
+  } else {
+    res.status(405).json({ error: "Method not allowed" });
+  }
+}
